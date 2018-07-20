e3f62c5a740b3fd300c10372f8f060bc3c1931b3
MacStudent <macstudent@20X-iMacXX-M-406.local>
2018-07-20 16:31:40 -0400

    start

---
+++ C0737616_Exam1_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0737616_Exam1_MAD3004
+//
+//  Created by MacStudent on 2018-07-20.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
